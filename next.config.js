632f05fce721b917028a7e3a19ba08d6c61e1648
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2022-11-23 15:28:10 +0100

    PWA init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  pwa: {
+    dest: "public",
+    register: true,
+    skipwaiting: true,
+    disable: process.env.NODE_ENV == "development",
+  },
+});
