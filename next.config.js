60aded60d1db50cad59a7b055dd8282307ba3065
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2022-11-24 02:35:07 +0100

    next config edit for destination cors error

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV == "development",
 });
-/** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+
+  source: "",
+  destination: "https://craftzdog.global.ssl.fastly.net/homepage",
 });
 
 module.exports = nextConfig;
