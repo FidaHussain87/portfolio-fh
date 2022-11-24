5cfe47aff7f6d5dce6d4bcfdc1f00c76377d3d20
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2022-11-24 02:36:37 +0100

    prod url for animation removed

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const withPWA = require("next-pwa")({
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-
-  source: "",
-  destination: "https://craftzdog.global.ssl.fastly.net/homepage",
 });
 
 module.exports = nextConfig;
